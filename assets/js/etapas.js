834598456fc34f55a3aec47d859f7a40c66c88e9
fernandoromeroalves <133946667+fernandoromeroalves@users.noreply.github.com>
2023-09-06 19:04:32 -0300

    controle das áreas da tela

---
+++ assets/js/etapas.js
@@ -0,0 +1,51 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos:[
+            {
+                numero:38111,
+                nome: 'Joseph Joestar "JOJO"',
+                partido: 'JoJo Bizarre Adventure',
+                fotos:[
+                    {url:'assets/image/38111.jpg',legenda: 'vereador', small: false}
+                ]
+            },
+            {
+                numero:77222,
+                nome:'Saitama',
+                partido: 'partido dos super herois',
+                fotos:[
+                    {url:'assets/image/77222.jpg', legenda: 'vereador'}
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+            nummero: 99,
+            nome: 'Goku',
+            partido:'dragon ball',
+            vice:'Monkey D. Luffy',
+            fotos:[
+                {url:'assets/image/99.jpg', legenda:'Prefeito'},
+                {url:'assets/image/99_2', legenda: 'Vice-Prefeito', small: true}
+            ]
+        },
+        {
+            numero:84,
+            nome:' Naruto Uzumaki',
+            partido: 'vila da folha',
+            vice: 'Ichigo Kurosaki',
+            fotos:[
+                {url:'assets/image/84.jpg', legenda: 'Prefeito'},
+                {url:'assets/image/84_2.jpg', legenda:'Vice-Prefeito'}
+            ]
+        }
+    ]
+    }
+
+]
